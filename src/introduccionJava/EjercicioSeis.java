04dac1d706731b9ddff48ea6906089bbd423d048
Camilo Quiroga <quirogacamilodev@gmail.com>
2025-08-25 21:37:55 -0300

    add tp1

---
+++ src/introduccionJava/EjercicioSeis.java
@@ -0,0 +1,8 @@
+package introduccionJava;
+
+public class EjercicioSeis {
+
+    public static void main(String[] args) {
+        System.out.println("Nombre: Juan Perez\nEdad: 30 años\nDireccion: \"Calle Falsa 123\"");
+    }
+}
